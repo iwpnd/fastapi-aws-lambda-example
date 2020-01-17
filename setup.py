3b2c26b6bbdb71554261bc2f41cd22a88e1dd8c0
iwpnd <legionaerr@googlemail.com>
2020-01-17 12:11:58 +0100

    removed poetry. add requirements.txt and setup.py instead. also added scripts folder for example.ipynb

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+packages = []
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name="fastapi_aws_lambda_example_api",
+    version="0.1.0",
+    description="example api to be deployed to aws lambda",
+    url="http://github.com/iwpnd/fastapi-aws-lambda-example",
+    author="probably you",
+    author_email="probably@you.pw",
+    license="MIT",
+    include_package_data=True,
+    install_requires=requirements,
+    packages=packages,
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+    ],
+)
